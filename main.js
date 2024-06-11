84ff384852cc84856326e7bdca2026f502363e29
Fernando Silva <artfernando.carlos@gmail.com>
2024-06-11 10:47:10 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,13 @@
+function nakedCover() {
+    document.getElementById("cover1").src = "/Imagens do site/Captura de tela 2024-06-06 161943.jpg"
+}
+function nakedNormal() {
+    document.getElementById("cover1").src = "/Imagens do site/naked-cover.png"
+}
+
+function enduroCover() {
+    document.getElementById("cover2").src = "/Imagens do site/Captura de tela 2024-06-06 162430.jpg"
+}
+function enduroNormal() {
+    document.getElementById("cover2").src = "/Imagens do site/Enduro-cover.png"
+}
